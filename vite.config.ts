82c7659b571a2e7ebeeafd3e9429966ee23a8149
yepitsaname <fernando.g.berry@gmail.com>
2025-09-14 18:05:01 -0400

    fixed dependency issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { coverageConfigDefaults } from 'vitest/config'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
