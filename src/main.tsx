bc2e4222eb67caf353bc1a8c6512cda55c30919f
joshkrutz <jfkrutz@gmail.com>
2025-09-14 15:14:28 -0700

    style

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRouter } from "./AppRouter";
+import { ThemeProvider } from "./components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
+    <ThemeProvider defaultTheme="dark">
+      <AppRouter />
+    </ThemeProvider>
   </StrictMode>
 );
