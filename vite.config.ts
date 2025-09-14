ec0512a732ec96caa2c0ab460cd8f066e2ec68d8
joshkrutz <jfkrutz@gmail.com>
2025-09-14 15:54:11 -0700

    Happy code

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import { coverageConfigDefaults } from 'vitest/config'
-// import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { coverageConfigDefaults } from "vitest/config";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-    // alias: {"@": path.resolve(__dirname, "./src")},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+  test: {
+    coverage: {
+      exclude: [...coverageConfigDefaults.exclude, "src/main.jsx"],
     },
-    test: {
-        coverage: {
-            exclude: [...coverageConfigDefaults.exclude, "src/main.jsx"],
-        },
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: "./test-setup.js"
-    }
-})
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./test-setup.js",
+  },
+});
