d279d0a21e921e0aa6524efe79ed144f728070d5
yepitsaname <fernando.g.berry@gmail.com>
2025-09-15 10:02:43 -0400

    more testing on sidebars

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,9 +1,56 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Sidebar, SidebarHeader, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "dashboard",
+    icon: Home,
+  },
+  // {
+  //   title: "Inbox",
+  //   url: "#",
+  //   icon: Inbox,
+  // },
+  // {
+  //   title: "Calendar",
+  //   url: "#",
+  //   icon: Calendar,
+  // },
+  // {
+  //   title: "Search",
+  //   url: "#",
+  //   icon: Search,
+  // },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
 
 export default function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarHeader>Black Relay</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   )
 }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,17 @@
 import AppSidebar from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
-import type React from "react";
+// import type React from "react";
 import { Outlet } from "react-router-dom";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout(/*{ children }: { children: React.ReactNode }*/) {
   return (
     <div>
       <SidebarProvider>
         <AppSidebar />
+        <h1>Layout details</h1>
         <SidebarTrigger/>
-        {children}
+        {/* {children} */}
       </SidebarProvider>
-      Layout details
       <Outlet />
     </div>
   );
